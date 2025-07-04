d734a8123d5e9fc714f11c43ae896a204bd7eb4a
emmidynamics <emmanuel.m2204207@st.futminna.edu.ng>
2025-07-04 14:05:16 +0100

    Update Kultra Mega Stores Inventory  SQL Code.sql

--- Kultra Mega Stores Inventory  SQL Code.sql
+++ Kultra Mega Stores Inventory  SQL Code.sql
@@ -2,7 +2,7 @@ SELECT *
 FROM [dbo].[KMS Sql Case Study]
 
 
---Which product category had the highest sales
+--1. Which product category had the highest sales
 
 SELECT Product_Category, SUM(sales) as highest_Sales
 FROM [dbo].[KMS Sql Case Study]
@@ -13,7 +13,7 @@ ORDER BY highest_Sales DESC
 
 
 
---What are the Top 3 
+--2. What are the Top 3 
 
 SELECT TOP 3 Region, SUM(Sales) AS Top3_Region
 FROM [dbo].[KMS Sql Case Study]
@@ -27,7 +27,7 @@ FROM [dbo].[KMS Sql Case Study]
 GROUP BY Region
 ORDER BY Top3_Region ASC
 
---What were the total sales of appliances in Ontario
+--3. What were the total sales of appliances in Ontario
 
 SELECT Region, SUM(Sales) AS TotalSales_in_Ontario
 FROM [dbo].[KMS Sql Case Study]
@@ -35,7 +35,7 @@ WHERE Region = 'Ontario'
 GROUP BY Region 
 
 
--- Advise the management of KMS on what to do to increase the revenue from the bottom 10 customers 
+--4. Advise the management of KMS on what to do to increase the revenue from the bottom 10 customers 
 ##first let known them 
 
 SELECT TOP 10 Customer_Name, SUM(Sales) AS Bottom10_Customers
@@ -44,7 +44,7 @@ GROUP BY Customer_Name
 ORDER BY Bottom10_Customers ASC
 
 
- --KMS incurred the most shipping cost using which shipping method
+ --5. KMS incurred the most shipping cost using which shipping method
 
  SELECT TOP 1 Ship_Mode, SUM(Shipping_Cost) AS shipping_Cost
 FROM [dbo].[KMS Sql Case Study]
@@ -53,13 +53,13 @@ GROUP BY Ship_Mode
 
 --CASE SCENARIO II 
 
---Who are the most valuable customers, and what products or services do they typically purchase? 
+--6. Who are the most valuable customers, and what products or services do they typically purchase? 
 
 SELECT TOP 10 Customer_Name,Product_Name, Sales AS Top10_Customers
 FROM [dbo].[KMS Sql Case Study]
 ORDER BY Top10_Customers DESC
 
---Which small business customer had the highest sales
+--7. Which small business customer had the highest sales
 
 SELECT TOP 1 Customer_Segment, Customer_Name,Sales AS  Higest_sales
 FROM [dbo].[KMS Sql Case Study]
@@ -67,7 +67,7 @@ WHERE Customer_Segment = 'small business'
 ORDER BY Higest_sales DESC
 
 
---Which Corporate Customer placed the most number of orders in 2009 – 2012
+--8. Which Corporate Customer placed the most number of orders in 2009 â€“ 2012
 
 --------i had to alter the column type>>>>>>>>
 ALTER TABLE [KMS Sql Case Study]
@@ -87,7 +87,7 @@ SELECT *
 FROM [dbo].[KMS Sql Case Study]
 
 
---Which consumer customer was the most profitable one
+--9. Which consumer customer was the most profitable one
 
 SELECT TOP 1 Customer_Name, MAX(profit) AS Most_Profitable
 FROM [dbo].[KMS Sql Case Study]
@@ -96,13 +96,13 @@ GROUP BY Customer_Name
 ORDER BY Most_Profitable DESC
 
 
----Which customer returned items, and what segment do they belong to
+---10. Which customer returned items, and what segment do they belong to
 
 SELECT Customer_Name, Customer_Segment
 FROM [dbo].[KMS Sql Case Study]
 WHERE Profit < 0 
 
----Customer from each Customer Segment 
+---lets know Customer from each Customer Segment 
 
 SELECT Customer_Segment, COUNT(Customer_Name) AS [No of Customers]
 FROM [dbo].[KMS Sql Case Study]
@@ -112,9 +112,7 @@ GROUP BY Customer_Segment
 
 SELECT *
 FROM [dbo].[KMS Sql Case Study]
--- If the delivery truck is the most economical but the slowest shipping method and 
---Express Air is the fastest but the most expensive one, do you think the company 
---appropriately spent shipping costs based on the Order Priority
+-- 11. If the delivery truck is the most economical but the slowest shipping method and Express Air is the fastest but the most expensive one, do you think the company appropriately spent shipping costs based on the Order Priority
 
 
 SELECT Order_Priority,AVG(Shipping_Cost) AS [shipping cost from delivery truck]
